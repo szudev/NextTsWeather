27e7615ba945396d7e5189c45c1ffe0384df730a
szudev <sz.ubiergo@gmail.com>
2022-06-05 01:40:23 -0400

    day commit point

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import { prisma } from "../db/client";
 import { trpc } from "../utils/trpc";
 
@@ -8,16 +7,6 @@ const Home: NextPage = (props: any) => {
 
   if (isLoading || !data) return <div>Loading...</div>;
   return <div>{data.greeting}</div>;
-};
-
-export const getServerSideProps = async () => {
-  const questions = await prisma.pollQuestion.findMany();
-
-  return {
-    props: {
-      questions: JSON.stringify(questions),
-    },
-  };
 };
 
 export default Home;
