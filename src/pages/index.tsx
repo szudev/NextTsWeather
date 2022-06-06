b7bfb5040f05ab4a3db8648b8ba6deaf85306b51
szudev <sz.ubiergo@gmail.com>
2022-06-05 22:19:15 -0400

    working trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,11 @@ import { prisma } from "../db/client";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = (props: any) => {
-  const { data, isLoading } = trpc.useQuery(["hi2"]);
+  const { data, isLoading } = trpc.useQuery(["GetAllQuestions"]);
+  console.log(data);
 
   if (isLoading || !data) return <div>Loading...</div>;
-  return <div>{data.greeting}</div>;
+  return <div>{data[0]?.question}</div>;
 };
 
 export default Home;
